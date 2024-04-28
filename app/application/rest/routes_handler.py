3116929fe06d9b8f983b537e844cc9ab04067984
manurnberg <manurnbergy@gmail.com>
2024-04-28 18:39:47 -0300

    update repository with DTOs

--- app/application/rest/routes_handler.py
+++ app/application/rest/routes_handler.py
@@ -1,11 +1,8 @@
 from flask import jsonify, request, Blueprint
-import json
 
 from domain.entities.character import Character
 from externals.repository.repository import Repository
 from serializers.DTOs.characterDTO import CharacterDTO
-from serializers.DTOs.charactersDTO import CharactersDTO
-from serializers.DTOs.new_characterDTO import NewCharacterDTO
 from domain.use_cases.use_cases import UseCases
 
 
@@ -20,57 +17,47 @@ use_case = UseCases(repository)
 def server_status():
     return jsonify({"response": "server running"}), 200
 
+
 ## get all characters route
 @blueprint.route('/characters', methods=['GET'])
 def get_characters():
     try:
         characters = use_case.get_all()
         if characters:
-            serialized_characters = [CharactersDTO(
-                id=character.id,
-                name=character.name,
-                height=character.height,
-                mass=character.mass,
-                birth_year=character.birth_year,
-                eye_color=character.eye_color
-            ).to_dict() for character in characters]
-            return jsonify(serialized_characters), 200
+            return jsonify(characters), 200
         else:
             return jsonify({'message': 'No characters stored'}), 404
     except Exception as e:
         return jsonify({'message': f'Error retrieving characters: {e}'}), 500
 
+
 ## get character by id route
 @blueprint.route('/characters/<int:id>', methods=['GET'])
-def get_character(id) -> CharacterDTO:
+def get_character(id):
     try:
         character = use_case.get_by_id(id)
-        
         if character:
-            character = Character.to_dict(character)
-            character_dto = CharacterDTO.from_dict(character)
-            return jsonify(character_dto.to_dict()), 200
+            return jsonify(character), 200
         else:
             return jsonify({'message': 'Character not found'}), 404
     except Exception as e:
         return jsonify({'message': f'Error retrieving character: {e}'}), 500
+
 
 ## add new character route
 @blueprint.route('/characters', methods=['POST'])
 def add_character():
     try:
         data = request.json
-        new_character_dto = NewCharacterDTO.from_dict(data)
-        new_character_dto = new_character_dto.to_dict()
-        db_character = use_case.get_by_attribute(new_character_dto['name'])
+        db_character = use_case.get_by_attribute(data['name'])
         if db_character:
             return jsonify({'message': 'Character already exists'}), 400
         else:     
-            new_character = Character.to_dict(use_case.add(new_character_dto))
-            serialized_character = CharacterDTO.from_dict(new_character)
-            return jsonify(serialized_character.to_dict()), 201
+            new_character = Character.to_dict(use_case.add(data))
+            return jsonify(new_character), 201
     except Exception as e:
         return jsonify({'message': f'Error creating character: {e}'}), 500
+
 
 ## delete a character by id route
 @blueprint.route('/characters/<int:id>', methods=['DELETE'])
--- app/externals/repository/repository.py
+++ app/externals/repository/repository.py
@@ -1,3 +1,6 @@
+from serializers.DTOs.characterDTO import CharacterDTO
+from serializers.DTOs.new_characterDTO import NewCharacterDTO
+from serializers.DTOs.charactersDTO import CharactersDTO
 from externals.database.models.character import Character
 from externals.database.models.character import db
 
@@ -7,36 +10,67 @@ class Repository:
     @staticmethod
     def get_all():
         try:
-            return Character.query.all()
+            characters = Character.query.all()
+            serialized_characters = []
+            for character in characters:
+                serialized_character = CharactersDTO(
+                    id=character.id,
+                    name=character.name,
+                    height=character.height,
+                    mass=character.mass,
+                    birth_year=character.birth_year,
+                    eye_color=character.eye_color
+                ).to_dict()
+                serialized_characters.append(serialized_character)
+            return serialized_characters
         except Exception as e:
             raise RuntimeError(f"Error al obtener todos los personajes: {e}")
+
 
     @staticmethod
     def get_by_id(id):
         try:
-            return Character.query.get(id)
+            character = Character.query.get(id)
+            if character:
+                character_dto = CharacterDTO(
+                    id=character.id,
+                    name=character.name,
+                    height=character.height,
+                    mass=character.mass,
+                    birth_year=character.birth_year,
+                    eye_color=character.eye_color,
+                    hair_color=character.hair_color,
+                    skin_color=character.skin_color,
+                )
+                return character_dto.to_dict()
+            else:
+                return None
         except Exception as e:
             raise RuntimeError(f"Error al obtener el personaje con ID {id}: {e}")
 
+
     @staticmethod
     def add(data):
+        new_character_dto = NewCharacterDTO.from_dict(data)
         try:
             new_character = Character(
-                name=data['name'],
-                height=data['height'],
-                mass=data['mass'],
-                hair_color=data['hair_color'],
-                skin_color=data['skin_color'],
-                eye_color=data['eye_color'],
-                birth_year=data['birth_year']
+                name=new_character_dto.name,
+                height=new_character_dto.height,
+                mass=new_character_dto.mass,
+                hair_color=new_character_dto.hair_color,
+                skin_color=new_character_dto.skin_color,
+                eye_color=new_character_dto.eye_color,
+                birth_year=new_character_dto.birth_year
             )
             db.session.add(new_character)
             db.session.commit()
+
             return new_character
         except KeyError as e:
             raise ValueError(f"Campo faltante en los datos: {e}")
         except Exception as e:
             raise RuntimeError(f"Error al agregar el personaje: {e}")
+        
 
     @staticmethod
     def delete(id):
